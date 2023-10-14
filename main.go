ce51246bb03d93c1abf5e73a146088258e589756
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-10-14 15:58:23 +0900

    \n

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s address", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s address\n", os.Args[0])
 		os.Exit(1)
 	}
 	resp, err := openstreetmap.Geocoder().Geocode(os.Args[1])
